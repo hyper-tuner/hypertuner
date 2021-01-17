da4350bc02d7f27e3c05aa5e901ab45fba4c5033
Piotr Rogowski <piotr.rogowski0@gmail.com>
2021-01-17 14:45:17 +0100

    Trigger pattern converter

--- bin/trigger.js
+++ bin/trigger.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 console.log('------------ start ------------');
 
 const miata_9905 = [
@@ -15,69 +18,64 @@ const miata_9905 = [
   0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, // Pulse at 640
   0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, // Pulse at 710
 ];
+ 
+const flattenPattern = (pattern) => {
+  const multiplayer = 720 / pattern.length;
+  const result = [];
 
-const generate = (pattern) => {
-  const maxDeg = 720;
-  const resolution = maxDeg / pattern.length;
-  const crank = [];
-  const cam = [];
-  let camLast = 0;
-  let crankLast = 0;
+  pattern.forEach((val) =>
+    result.push(...(new Array(multiplayer).fill(val))));
 
-  [...Array(maxDeg).keys()].forEach((index) => {
-
-    if (index === 0 || index % resolution === 0) {
-      const patternIndex = index / resolution;
-      const current = pattern[patternIndex];
-
-      switch (current) {
-        case 0:
-          crank[index] = 0;
-          cam[index] = 0;
-          crankLast = 0;
-          camLast = 0;
-          break;
-        case 1:
-          crank[index] = 1;
-          crankLast = 1;
-          break;
-        case 2:
-          cam[index] = 1;
-          camLast = 1;
-          break;
-        case 3:
-          crank[index] = 1;
-          cam[index] = 1;
-          crankLast = 1;
-          camLast = 1;
-          break;
-        default:
-          throw new Error(`Invalid pattern [${current}] at: ${patternIndex}`);
-      }
-
-      return;
-    }
-
-    // TODO: count length of a tooth
-    crank[index] = 0;
-    cam[index] = 0;
-  });
-
-  return {
-    crank,
-    cam,
-  };
+  return result;
 };
 
-const { crank, cam } = generate(miata_9905);
+const splitPattern = (pattern) => {
+  const result = {
+    crank: new Array(720).fill(0),
+    cam: new Array(720).fill(0),
+  };
 
-console.log('== crank ==');
-console.log(
-  crank.map((val, index) => val ? '▔' : '.' ).join(''),
+  pattern.forEach((val, index) => {
+    switch (val) {
+      case 0:
+        result.crank[index] = 0;
+        result.cam[index] = 0;
+        break;
+      case 1:
+        result.crank[index] = 1;
+        break;
+      case 2:
+        result.cam[index] = 1;
+        break;
+      case 3:
+        result.crank[index] = 1;
+        result.cam[index] = 1;
+        break; 
+      default:
+        throw new Error(`Invalid value [${val}] at index [${index}]`);
+    }
+  });
+
+  return result;
+};
+
+// flat
+console.dir(flattenPattern(miata_9905), { maxArrayLength: null });
+
+// split
+// console.dir(splitPattern(flattenPattern(miata_9905)), { maxArrayLength: null });
+
+// csv out flat
+fs.writeFileSync(
+  path.join(__dirname, 'trigger_flat.csv'),
+  flattenPattern(miata_9905).join(','),
 );
-console.log('== cam ==');
-console.log(
-  cam.map((val, index) => val ? '▔' : '.' ).join(''),
+
+// csv out split
+const { crank, cam } = splitPattern(flattenPattern(miata_9905));
+fs.writeFileSync(
+  path.join(__dirname, 'trigger_split.csv'), 
+  [crank.join(','), cam.join(',')].join('\n'),
 );
 
 console.log('------------ end ------------');
