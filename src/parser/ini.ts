4db5e2952b9a8e16962157478f485f04bd8cc14f
Piotr Rogowski <piotr.rogowski0@gmail.com>
2021-03-07 19:17:01 +0100

    Add JSON config

--- src/parser/ini.ts
+++ src/parser/ini.ts
@@ -618,13 +618,16 @@ class Parser {
   static sanitizeCondition = (val: string) => val.replace(/^{\s*|\s*}$/g, '').trim();
 }
 
+const version = 202012;
+// const version = 202103;
+
 const result = new Parser(
-  fs.readFileSync(path.join(__dirname, '/../../public/tunes/202103.ini'), 'utf8'),
+  fs.readFileSync(path.join(__dirname, `/../../public/tunes/${version}.ini`), 'utf8'),
 ).parse();
 
-// console.log('Dupa');
 // console.dir(result.pcVariables, { maxArrayLength: 10, depth: null });
 
-fs.writeFileSync(path.join(__dirname, '/../../public/tunes/202103.yml'), yaml.dump(result));
+fs.writeFileSync(path.join(__dirname, `/../../public/tunes/${version}.yml`), yaml.dump(result));
+fs.writeFileSync(path.join(__dirname, `/../../public/tunes/${version}.json`), JSON.stringify(result));
 
 console.log('------- end --------');
